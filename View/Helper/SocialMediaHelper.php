2c9bb7a055daeb57900d960891d7144636f976cb
Mischa ter Smitten <mischa@tersmitten.nl>
2013-07-09 11:21:12 +0200

    Made the API more consistent and added the possibility to pass an image as a link

--- View/Helper/SocialMediaHelper.php
+++ View/Helper/SocialMediaHelper.php
@@ -31,45 +31,53 @@ class SocialMediaHelper extends AppHelper {
 /**
  * Creates a Facebook feed dialog link.
  *
- * ### Options
+ *  - Url parameters:
+ *    - `app_id` Your application's identifier. Defaults to the configured SocialMedia.facebookAppId
+ *    - `redirect_uri` The URL to redirect to after the user clicks a button on the dialog. Defaults to the current url.
+ *    - `link` The link attached to this post. Defaults to the current url.
+ *    - `picture` The URL of a picture attached to this post.
+ *                The picture must be at least 50px by 50px (though minimum 200px by 200px is preferred)
+ *                and have a maximum aspect ratio of 3:1
+ *    - `name` The name of the link attachment.
+ *    - `caption` The caption of the link (appears beneath the link name).
+ *                If not specified, this field is automatically populated with the URL of the link.
+ *    - `description` The description of the link (appears beneath the link caption).
+ *                    If not specified, this field is automatically populated by information scraped from the link,
+ *                    typically the title of the page.
  *
- * - `app_id` Your application's identifier. Defaults to the configured SocialMedia.facebookAppId
- * - `redirect_uri` The URL to redirect to after the user clicks a button on the dialog. Defaults to the current url.
- * - `link` The link attached to this post. Defaults to the current url.
- * - `picture` The URL of a picture attached to this post.
- *             The picture must be at least 50px by 50px (though minimum 200px by 200px is preferred)
- *             and have a maximum aspect ratio of 3:1
- * - `name` The name of the link attachment.
- * - `caption` The caption of the link (appears beneath the link name).
- *             If not specified, this field is automatically populated with the URL of the link.
- * - `description` The description of the link (appears beneath the link caption).
- *                 If not specified, this field is automatically populated by information scraped from the link,
- *                 typically the title of the page.
+ *  - Options:
+ *    - `escape` Set to false to disable escaping of title and attributes.
  *
  * @param string $title The content to be wrapped by <a> tags.
- * @param array $options An array of options.
+ * @param array $urlParameters An array of URL parameters
+ * @param array $options An array of HTML attributes.
+ * @param string $confirmMessage A javaScript confirmation message.
  * @return string An `<a />` element.
  */
-	public function facebook($title, $options = array()) {
+	public function facebook($title, $urlParameters = array(), $options = array(), $confirmMessage = false) {
 		$defaults = array(
 			'app_id' => Configure::read('SocialMedia.facebookAppId'),
 			'redirect_uri' => $this->Html->url('', true),
 			'link' => $this->Html->url('', true)
 		);
-		$options = array_merge($defaults, $options);
+		$urlParameters = array_merge($defaults, $urlParameters);
 
 		return $this->Html->link(
-			$title, SocialMediaHelper::FACEBOOK_FEED_DIALOG_URL . '?' . http_build_query($options)
+			$title, SocialMediaHelper::FACEBOOK_FEED_DIALOG_URL . '?' . http_build_query($urlParameters), $options, $confirmMessage
 		);
 	}
 
 /**
  * Creates a Hyves Respect HTML link.
  *
- * ### Options
+ *  - Url parameters:
+ *    - `title` The title of the link.
+ *    - `url` The link attached to this post.
+ *    - `description` The description of the link.
+ *    - `image` The URL of a picture attached to this post.
  *
- * - `escape` Set to false to disable escaping of title and attributes.
- * - `confirm` JavaScript confirmation message.
+ *  - Options:
+ *    - `escape` Set to false to disable escaping of title and attributes.
  *
  * @param string $title The content to be wrapped by <a> tags.
  * @param array $urlParameters An array of URL parameters
