7f2ebc0d2573bd0664ddf0df25bb85d4e081e1f4
Mischa ter Smitten <mischa@tersmitten.nl>
2018-01-19 20:54:28 +0100

    Fix phpunit tests in standalone mode
    fixes #14

---
+++ tests/test_app/config/routes.php
@@ -0,0 +1,13 @@
+<?php
+use Cake\Core\Plugin;
+use Cake\Routing\RouteBuilder;
+use Cake\Routing\Router;
+use Cake\Routing\Route\DashedRoute;
+
+Router::defaultRouteClass(DashedRoute::class);
+
+Router::scope('/', function (RouteBuilder $routes) {
+    $routes->fallbacks(DashedRoute::class);
+});
+
+Plugin::routes();
---
+++ tests/test_app/src/Controller/AppController.php
@@ -0,0 +1,12 @@
+<?php
+namespace SocialMedia\Test\TestApp\Controller;
+
+use Cake\Controller\Controller;
+
+/**
+ * Application Controller
+ *
+ */
+class AppController extends Controller
+{
+}
